9eec299fd6949cdbca2d3c0b5272baa57c014862
scott88888 <pig1980813@gmail.com>
2023-05-24 16:43:25 +0800

    更新出貨查詢

--- resources/views/mesShipmentList.blade.php
+++ resources/views/mesShipmentList.blade.php
@@ -140,10 +140,7 @@
         language: {
             search: 'Filter' // 將搜尋文字改為中文
         },
-        dom: 'Blfrtip',
-        buttons: [
-            'csvHtml5'
-        ],
+
         lengthMenu: [
             [10, 25, 50, -1],
             [10, 25, 50, "All"]
