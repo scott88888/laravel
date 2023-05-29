29791f060d4323dfec8595e95e2ea2a540f44e2d
scott88888 <pig1980813@gmail.com>
2023-05-29 09:39:49 +0800

    調整首頁css到server

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,8 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="icon" type="image/png" href="{{ asset('images/icon/lilinlogo.png') }}">
+        <link rel="stylesheet" href="{{ asset('css/family_figtree.css') }}">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
