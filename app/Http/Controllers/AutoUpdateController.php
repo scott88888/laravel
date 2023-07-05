bf6069e784ba0f7f7e1c9ff8c97c0b1e1cfabbec
scott88888 <pig1980813@gmail.com>
2023-07-05 09:48:33 +0800

    儀錶板自動更新

--- app/Http/Controllers/AutoUpdateController.php
+++ app/Http/Controllers/AutoUpdateController.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 
-
+use Throwable;
 use DB;
 use Illuminate\Http\Request;
 
@@ -17,36 +17,57 @@ class AutoUpdateController extends BaseController
 
     public function mesAutoUpdate(Request $request)
     {
-        // 讀取資料
 
+
+        
         $date = date('ymd');
-        $data = 'MR'.$date.'%';
+        $data = 'MR' . $date . '%';
+        // 讀取資料
         $runcard_ng_rate = DB::connection('mysql_second')
             ->table('runcard_ng_rate')
             ->where('num_comr', 'LIKE', $data)
             ->get();
-        // 寫入資料
-        foreach ($runcard_ng_rate as $data) {
-            DB::table('runcard_ng_rate')->insert([
-                'ng_id' => $data->ng_id,
-                'num_comr' => $data->num_comr,
-                'work_no' => $data->work_no,
-                'order_qty' => $data->order_qty,
-                'ng_qty' => $data->ng_qty,
-                'ng_rate' => $data->ng_rate,
-                'runcard_no' => $data->runcard_no,
-                'pp_qty' => $data->pp_qty,
-                'model' => $data->model,
-                'version' => $data->version,
-                'comr_user' => $data->comr_user,
-                'cod_qc' => $data->cod_qc,
-                'sts_comr' => $data->sts_comr
-            ]);
+        if ($runcard_ng_rate->count() != 0) {
+            DB::table('runcard_ng_rate')->where('num_comr', 'LIKE', $data)->delete();            
+            foreach ($runcard_ng_rate as $data) {
+                try {
+                    DB::table('runcard_ng_rate')->insert([
+                        'ng_id' => $data->ng_id,
+                        'num_comr' => $data->num_comr,
+                        'work_no' => $data->work_no,
+                        'order_qty' => $data->order_qty,
+                        'ng_qty' => $data->ng_qty,
+                        'ng_rate' => $data->ng_rate,
+                        'runcard_no' => $data->runcard_no,
+                        'pp_qty' => $data->pp_qty,
+                        'model' => $data->model,
+                        'version' => $data->version,
+                        'comr_user' => $data->comr_user,
+                        'cod_qc' => $data->cod_qc,
+                        'sts_comr' => $data->sts_comr
+                    ]);
+                } catch (Throwable $e) {
+                    // 發生錯誤時的處理邏輯
+                    $msg='錯誤'.date('His').$e->getMessage();
+                    $this->writeLog($msg);
+                }
+            }
+        }
+    }
+
+
+    public function writeLog($msg)
+    {
+        $date = date('ymd');
+
+        $logFileDirectory = 'log/'; // 目錄路徑
+        $logFilePath = $logFileDirectory . $date . '.txt'; // 完整檔案路徑        
+        // 建立目錄
+        if (!is_dir($logFileDirectory)) {
+            mkdir($logFileDirectory, 0777, true); // 第三個參數設為 true 可以遞迴地建立目錄
         }
 
-        // $value = DB::table('runcard_ng_rate')->where('num_comr', 'LIKE', 'MR230620%')->get();
-
-        // $runcard_ng_rate = DB::connection('mysql_second')->table('runcard_ng_rate')->where('num_comr', 'LIKE', 'MR230620%')->get();
-        // var_dump($value);
+        // 建立檔案並寫入內容
+        file_put_contents($logFilePath, $msg . PHP_EOL, FILE_APPEND);
     }
 }
